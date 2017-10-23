56504f190b356c630baea163ec7c4c25e0458ee1
jacquelineStella <31634683+jacquelineStella@users.noreply.github.com>
2017-10-23 14:17:31 -0300

    validacion de registro

--- interfaz/zip/js/registrar.js
+++ interfaz/zip/js/registrar.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
         3. Envia los datos
         4. Confirma registro al usario
     */
-    $("#btn_registro").click(function(){
+    $("#enviar").click(function(){
         var usuario = validar_datos_registro();
         var recurso = "usuario/registro/"
 
@@ -48,8 +48,65 @@ $(document).ready(function(){
 
     function validar_datos_registro() {
         // captura los datos de los inputs y validar los datos
+       
         var email = $("#email_registro").val();
         var password = $("#password-1_registro").val();
+        var password2 = $("#password-2_registro").val();
+        var text1 = document.getElementById("text1");
+        var text2 = document.getElementById("text2");
+        var text3 = document.getElementById("text3");
+        var expMail= /^[0-9a-zA-Z._.-]+\@[0-9a-zA-Z._.-]+\.[0-9a-zA-Z]+$/;
+        var exp= /(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{8,})$/;
+
+        if (email==""){
+            text1.innerHTML="*Debe completar el email";
+            InsertarClaseError("text1");
+            return false;
+
+        }else if((expMail.test(email) == 0) || (email.length = 0)){
+            text1.innerHTML="*Debe completar un email valido";
+            InsertarClaseError("text1");
+            return false;
+        }else{
+            EliminarClaseError("text1");
+            text1.innerHTML="";
+        }
+
+        if (password==""){
+            text2.innerHTML="*Debe completar la contraseña";
+            InsertarClaseError("text2");
+            return false;
+
+        }else if((exp.test(password) == 0) || (password.length = 0)){
+            text2.innerHTML="*Debe completar una contraseña con numeros y letras";
+            InsertarClaseError("text2");
+            return false;
+        }else{
+            EliminarClaseError("text2");
+            text1.innerHTML="";
+        }
+
+        if (password2==""){
+            text3.innerHTML="*Debe repetir la contraseña";
+            InsertarClaseError("text3");
+            return false;
+
+        }else if(password != password2){
+            text3.innerHTML="*Los contraseñas no coinciden ";
+            InsertarClaseError("text3");
+            return false;
+        }else{
+            EliminarClaseError("text3");
+            text3.innerHTML="";
+        }
+
+        
+
+
+
+
+
+
 
 
         // Crear una array con los datos formato: {'nombre', 'apellido', 'email', 'telefono', 'password'}
@@ -60,6 +117,21 @@ $(document).ready(function(){
         return datos;
     }
 
+    function InsertarClaseError(id){
+        var elemento = document.getElementById(id);
+
+        if(!elemento.classList.contains("error")){
+            elemento.classList.add("error");
+        }
+    }
+
+    function EliminarClaseError(id){
+        var elemento = document.getElementById(id);
+
+        if(elemento.classList.contains("error")){
+            elemento.classList.remove("error");
+        }
+    }    
 
 
 });
